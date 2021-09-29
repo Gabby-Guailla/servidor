139319fdd623b838fd2d7d639e59b931d73d9334
Gabby-Guailla <77037147+Gabby-Guailla@users.noreply.github.com>
2021-09-29 15:30:58 +0200

    Update index.js

--- static/js/index.js
+++ static/js/index.js
@@ -4,29 +4,28 @@ function LED1_On() {
   console.log("led on");
   //document.getElementById("sensor").innerHTML="led on";
   message = new Paho.MQTT.Message("ON");
-      message.destinationName = "lourdes.guailla@unach.edu.ec/t2";
-      client.send(message);
-  
+      message.destinationName = "lourdes.guailla@unach.edu.ec/t1";
+      client.send(message);  
 }
 function LED1_Off(){  
   //alert("led off");
   console.log("led off");
   message = new Paho.MQTT.Message("OFF");
-      message.destinationName = "lourdes.guailla@unach.edu.ec/t2";
+      message.destinationName = "lourdes.guailla@unach.edu.ec/t1";
       client.send(message);
   //document.getElementById("sensor").innerHTML="led off";
 }
-
-
-
-
-
-
+function LED1_pause(){  
+  //alert("led pause");
+  console.log("led pause");
+  message = new Paho.MQTT.Message("PAUSE");
+      message.destinationName = "lourdes.guailla@unach.edu.ec/t1";
+      client.send(message);
+  //document.getElementById("sensor").innerHTML="led off";
+}
 // Create a client instance
   //client = new Paho.MQTT.Client("postman.cloudmqtt.com", 14970);
-  
   client = new Paho.MQTT.Client("maqiatto.com", 8883, "web_" + parseInt(Math.random() * 100, 10)); 
-
   // set callback handlers
   client.onConnectionLost = onConnectionLost;
   client.onMessageArrived = onMessageArrived;
@@ -37,36 +36,31 @@ function LED1_Off(){
     onSuccess:onConnect,
     onFailure:doFail
   }
-
   // connect the client
   client.connect(options);
-   
   // called when the client connects
   function onConnect() {
     // Once a connection has been made, make a subscription and send a message.
     console.log("Conectado...");
-  
-    client.subscribe("lourdes.guailla@unach.edu.ec/t1");
+    client.subscribe("lourdes.guailla@unach.edu.ec/t2");
     message = new Paho.MQTT.Message("hola desde la web");
-    message.destinationName = "lourdes.guailla@unach.edu.ec/t2";
+    message.destinationName = "lourdes.guailla@unach.edu.ec/t1";
     client.send(message);
-  
   }
-
   function doFail(e){
     console.log(e);
-  
   }
-
   // called when the client loses its connection
   function onConnectionLost(responseObject) {
     if (responseObject.errorCode !== 0) {
       console.log("onConnectionLost:"+responseObject.errorMessage);
     }
   }
-
   // called when a message arrives
   function onMessageArrived(message) {
     console.log("onMessageArrived:"+message.payloadString);
-    document.getElementById("sensor").innerHTML=message.payloadString;
+    var sms=message.payloadString.split(';') 
+    document.getElementById("sensor").innerHTML=sms[0];
+    document.getElementById("sensor2").innerHTML=sms[1];
+    //document.getElementById("sensor").innerHTML=message.payloadString;
   }
